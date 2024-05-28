258362e1622381e200a548398774a5eac368b8c8
Laxmikant Mapare <98962825+LakshMapare@users.noreply.github.com>
2024-05-28 13:24:59 +0530

    Update Constant.jsx

--- src/constants/Constant.jsx
+++ src/constants/Constant.jsx
@@ -12,7 +12,7 @@ import react from "../assets/React.png";
 import nextjs from "../assets/nextjs.png";
 
 import Hackthechain from "../assets/HackTheChainLogo.png";
-import GSSOC from "../assets/GSSoc.jpg";
+import GSSOC from "../assets/GSSoC.jpg";
 import IITB from "../assets/IITB.png";
 
 import IIITians from "../assets/IIITians_Network.png";
